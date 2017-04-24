81a928bd59470dae7536844d8c6fa05d04f8c73e
Long Nguyen <long@nomadreact.com>
2017-04-24 11:49:21 +0700

    Init commit

---
+++ src/index.test.js
@@ -0,0 +1,18 @@
+import hello from './index';
+
+test('supports async functions', async () => {
+  const result = await hello({ name: 'world' });
+  expect(result).toBe('hello world');
+});
+
+test('supports destructuring', async () => {
+  const { foo, ...rest } = { foo: 'bar', fiz: 'fuz' };
+  expect(foo).toBe('bar');
+  expect(rest).toEqual({ fiz: 'fuz' });
+});
+
+test('supports object spread', async () => {
+  const { fiz, foo } = { fiz: 'fuz', foo: 'bar' };
+  expect(foo).toEqual('bar');
+  expect(fiz).toEqual('fuz');
+});
